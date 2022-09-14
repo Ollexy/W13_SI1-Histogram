180cc20bfeaf0678919b4317accdfb2ce9b83b91
rafal <rafal.oleksiewicz@motorolasolutions.com>
2022-09-14 20:38:33 +0200

    updated histogram tests

--- HistogramTest/HistogramTest.cpp
+++ HistogramTest/HistogramTest.cpp
@@ -24,40 +24,49 @@ namespace HistogramTest
 
 	TEST(HistogramTest, TestInGivenRange) {
 		Histogram histogram;
-		std::vector<Range>range = histogram.generateRanges(1, 5);
+		std::vector<Range>range = histogram.generateRanges(1, 6);
+		std::map<Range, int> map = histogram.generate("siema", range);
 		EXPECT_NO_THROW(histogram.generate("hello",range));
 	}
 
 	TEST(HistogramTest, TestOutOfRange) {
 		Histogram histogram;
 		std::vector<Range>range = histogram.generateRanges(1, 6);
+		std::map<Range, int> map = histogram.generate("siema", range);
 		EXPECT_NO_THROW(histogram.generate("hello", range));
 	}
 
 	TEST(HistogramTest, TestEmptyText) {
 		Histogram histogram;
-		std::vector<Range>range = histogram.generateRanges(1, 5);
+		std::vector<Range>range = histogram.generateRanges(1, 6);
+		std::map<Range, int> map = histogram.generate("siema", range);
 		EXPECT_NO_THROW(histogram.generate("", range));
 	}
 
-	/*TEST(HistogramTest, TestHistogramMultipleTimes) {
+	TEST(histogramtest, testHistogramMultipleTimes) {
 		Histogram histogram;
 		std::vector<Range>range = histogram.generateRanges(1, 5);
-		EXPECT_TRUE(histogram.generate("hello", range));
-	}*/
+		std::map<Range, int> mapka = histogram.generate("siema", range);
+		histogram.generate("siema", range);
+		histogram.generate("siema", range);
+		EXPECT_NO_THROW(histogram.generate("hello", range));
+	}
+
 	TEST(HistogramTest, TestCallingBeforeGeneratingHistogram) {
 		Histogram histogram = Histogram();
-		EXPECT_EQ(histogram.draw(), "");
+		auto mapka = histogram.getHistogram();
+		EXPECT_TRUE(mapka.empty());
 	}
 
 	TEST(HistogramTest, TestCallingAfterGeneratingHistogram) {
 		Histogram histogram = Histogram();
-		EXPECT_EQ(histogram.draw(), "");
+		//przeiterowac przez mape zeby sprawdzic czy ma wszystkie elementy
+		EXPECT_EQ(histogram.getHistogram(), "");
 	}
 
 	/*TEST(HistogramTest, TestCallingAfterMultipleCalls) {
 		Histogram histogram = Histogram();
-		EXPECT_EQ(histogram.draw(), "");
+		EXPECT_EQ(histogram.getHistogram(), "");
 	}*/
 
 	//TEST(HistogramTest, TestGetMin) {
